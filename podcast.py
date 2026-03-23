58fef976904121f0fa08e027c82ceca8a87c5e8b
Thn347 <thn5129@psu.edu>
2026-03-23 15:11:22 -0400

    Created podcast.py = truck.py

---
+++ podcast.py
@@ -0,0 +1,35 @@
+from music_track import MusicTrack
+from artist import Artist
+from album import Album
+
+class Podcast(MusicTrack):
+    """
+    Represent a podcast
+    """
+
+    # constructor
+    def __init__(self,
+                 artist: Artist,
+                 album: Album,
+                 duration_seconds: float,
+                 is_explicit: bool = False # Additional attribution
+    ):
+        super().__init__(artist, album, duration_seconds)
+        self._is_explicit = is_explicit
+
+    @property
+    def is_explicit(self) -> bool:
+        return self._is_explicit
+
+    # specifying the abstract method with podcast's condition
+    def play_time_formatted(self) -> int:
+        hours = self._duration_seconds // 3600
+        minutes = self._duration_seconds // 60
+        seconds = self._duration_seconds % 60
+        return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+    
+    # printing podcast
+    def __str__(self) -> str:
+        return (
+            f"({self._artist}) {self._album}, duration: {self.play_time_formatted()} is explicit: {self._is_explicit}"
+        )
